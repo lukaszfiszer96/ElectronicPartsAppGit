de1af1375108582424869bb6dc46b156d147f756
lukaszfiszer96 <wkret96pl@gmail.com>
2018-11-10 14:27:15 +0100

    Adding unit Test
    Unit test for returning correct object filter by category

--- ElectronicParts.UnitTests/UnitTest1.cs
+++ ElectronicParts.UnitTests/UnitTest1.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ElectronicParts.WebUI.Controllers;
+using Moq;
+using ElectronicParts.Domain.Abstract;
+using ElectronicParts.Domain.Concrete;
+using ElectronicParts.Domain.Entities;
+using System.Linq;
+using ElectronicParts.WebUI.Models;
+using System.Web.Mvc;
+using System.Collections;
 
 namespace ElectronicParts.UnitTests
 {
@@ -7,8 +16,27 @@ namespace ElectronicParts.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Filter_Product()
         {
+            Mock<IElectronicPartRepository> mock = new Mock<IElectronicPartRepository>();
+            mock.Setup(m => m.ElectronicParts).Returns(new ElectronicPart[] 
+            {
+                new ElectronicPart{ElectronicPartID=1,Category="Category1" },
+                new ElectronicPart{ElectronicPartID=2,Category="Category1" },
+                new ElectronicPart{ElectronicPartID=3,Category="Category2" },
+                new ElectronicPart{ElectronicPartID=4,Category="Category2" }
+            });
+
+            ElectronicPartController controler = new ElectronicPartController(mock.Object);
+
+            ElectronicPart[] result = ((ElectronicPartViewModel)controler.List("Category1",null).Model).EleParts.ToArray();
+
+            Assert.AreEqual(result.Length,2);
+            Assert.IsTrue(result[0].ElectronicPartID == 1 && result[0].Category == "Category1");
+            Assert.IsTrue(result[1].ElectronicPartID == 2 && result[1].Category == "Category1");
+
+
+
         }
     }
 }
